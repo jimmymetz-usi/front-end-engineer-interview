505ca571543b541516da01afa53da1f70383a2b0
Jimmy Metz <jmetz@ultramain.com>
2022-12-13 11:46:37 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import ErrorPage from './components/ErrorPage';
 import HomePage from './components/HomePage';
 import UserPostsPage from './components/UserPostsPage';
-// test
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
